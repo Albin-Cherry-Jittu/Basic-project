357ab50ab5cebffb1be194cbc84d4e2fb2cce74a
Albin-Cherry-Jittu <albin.cherry1995@gmail.com>
2021-08-15 17:10:33 +0530

    loops

---
+++ js/loops/multioffour.js
@@ -0,0 +1,8 @@
+/*
+program to print multiple of 4 using while loop
+*/
+let i = 4;
+while (i <= 40) {
+  console.log(i);
+  i = i + 4;
+}
---
+++ js/loops/multiofsix.js
@@ -0,0 +1,8 @@
+/*
+program to print multiple of six using while loop
+*/
+let i = 6;
+while (i <= 60) {
+  console.log(i);
+  i = i + 6;
+}
