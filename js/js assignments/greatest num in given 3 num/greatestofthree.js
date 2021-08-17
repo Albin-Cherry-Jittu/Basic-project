d0a5f8711d1396e76ee8b1d35f208579677161cc
Albin-Cherry-Jittu <albin.cherry1995@gmail.com>
2021-08-18 02:03:11 +0530

    assignment

---
+++ js/js assignments/greatest num in given 3 num/greatestofthree.js
@@ -0,0 +1,11 @@
+//program to check  whish number is greatest of 3
+var a = 20;
+var b = 50;
+var c = 40;
+if (a > b) {
+    console.log(a, ' is greatest');
+} else if (b > c) {
+    console.log(b, ' is greatest');
+} else if (c > a) {
+    console.log(c, 'is greatest');
+}
---
+++ js/js assignments/greatest num/greatestnum.js
@@ -0,0 +1,8 @@
+//program to check  whish number is greatest of 2
+var a = 20;
+var b = 30
+if (a > b) {
+    console.log(a, ' is greatest');
+} else {
+    console.log(b, ' is greatest');
+}
