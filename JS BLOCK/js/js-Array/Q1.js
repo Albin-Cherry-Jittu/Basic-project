fa04b728372fdef73dd9bb1e274514058e96f9ee
Albin-Cherry-Jittu <albin.cherry1995@gmail.com>
2021-08-22 21:18:11 +0530

    arrays keys

---
+++ JS BLOCK/js/js-Array/Q1.js
@@ -0,0 +1,5 @@
+let size = ['albin', 'nicu', 'verma'];
+console.log(size.push('ashwin'));
+
+console.log(size);
+// push() method - add element/value end of your array and return new lengths
---
+++ JS BLOCK/js/js-Array/one.js
@@ -0,0 +1,4 @@
+let emp = ["ramesh", "mahesh", "suresh"];
+let x = emp.pop();
+console.log(emp);
+console.log(x);
